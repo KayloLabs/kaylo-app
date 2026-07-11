44ac8d9f3cd3508548d530f0f8e1f98d4eb4dfe3
nimaldanyathk <letsdotechndk@gmail.com>
2026-07-11 12:00:00 +0530

    feat(core): add app spacing tokens

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,9 @@
+class AppSpacing {
+  static const double xs = 4.0;
+  static const double s = 8.0;
+  static const double m = 12.0;
+  static const double l = 16.0;
+  static const double xl = 20.0;
+  static const double xxl = 24.0;
+  static const double xxxl = 32.0;
+}
